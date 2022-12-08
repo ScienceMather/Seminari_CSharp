ad261f2d3550af7c38360647b408d52ea2e24446
SCXBelka <scvifixbelka@gmail.com>
2022-12-08 22:08:12 +0900

    EShe_ispravl

---
+++ Zadacha_001/Program.cs
@@ -0,0 +1,9 @@
+﻿//program_kvadrata
+Console.Clear();
+
+Console.Write("Vvedite_chislo: ");
+int number=int.Parse(Console.ReadLine());
+
+int kvadrat = number*number;
+Console.WriteLine($"Kvadrat {number} ravno: {kvadrat}");
+//komentarii
