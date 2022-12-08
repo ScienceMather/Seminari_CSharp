00b74302b6d095f56dd63aac1855117bb30afff0
SCXBelka <scvifixbelka@gmail.com>
2022-12-08 23:46:35 +0900

    zadacha_proverki_kvadratov

---
+++ Zadacha_002/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Vvedite_chislo A: ");
+int number_A=int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite_chislo B: ");
+int number_B=int.Parse(Console.ReadLine());
+
+if (number_A==Math.Pow(number_B,2))
+{
+    Console.Write($"Chislo {number_A} kvadrat chisla {number_B}");
+}
+else
+{
+    Console.Write($"Chislo {number_A} ne kvadrat chisla {number_B}");
+}
