ee98f76b4452c2db2d37872fb589163797fbbd32
SCXBelka <scvifixbelka@gmail.com>
2022-12-12 23:43:30 +0900

    dob operator ili

--- Zadacha_108/Program.cs
+++ Zadacha_108/Program.cs
@@ -2,12 +2,11 @@
 // является ли одно число квадратом другого.
 
 Console.Write("Vvedite_chislo: ");
-int number1=int.Parse(Console.ReadLine());
+int num1=int.Parse(Console.ReadLine());
 
 Console.Write("Vvedite_chislo: ");
-int number2=int.Parse(Console.ReadLine());
+int num2=int.Parse(Console.ReadLine());
 int result = 0;
-if (number1*number1==number2) result =1;
-if (number2*number2==number1) result =1;
+if (num1*num1==num2 || num2*num2==num1) result =1;
 if (result==1) Console.WriteLine("Odno chislo kvadrat drygogo");
 else Console.WriteLine("Chisla ne peresekautsya kvadratami!");
