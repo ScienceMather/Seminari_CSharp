195994e7258163406f6e81433e22f2bf1931acb5
SCXBelka <scvifixbelka@gmail.com>
2022-12-12 23:36:38 +0900

    zadacha pro kvadrati

---
+++ Zadacha_108/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет,
+// является ли одно число квадратом другого.
+
+Console.Write("Vvedite_chislo: ");
+int number1=int.Parse(Console.ReadLine());
+
+Console.Write("Vvedite_chislo: ");
+int number2=int.Parse(Console.ReadLine());
+int result = 0;
+if (number1*number1==number2) result =1;
+if (number2*number2==number1) result =1;
+if (result==1) Console.WriteLine("Odno chislo kvadrat drygogo");
+else Console.WriteLine("Chisla ne peresekautsya kvadratami!");
