c8cb81e28135248131f74e320276c41479510c20
SCXBelka <scvifixbelka@gmail.com>
2022-12-12 23:26:31 +0900

    zadacha pro delimost na 7 i 23

---
+++ Zadacha_107/Program.cs
@@ -0,0 +1,9 @@
+﻿// Напишите программу, которая принимает на вход число и проверяет,
+// кратно ли оно одновременно 7 и 23.
+Console.Write("Vvedite_chislo: ");
+int num = int.Parse(Console.ReadLine());
+if (num%7==0)
+{
+    if (num%23==0) Console.WriteLine("Chislo delitsya nacelo na 7 i na 23!");
+}
+else Console.WriteLine("Chislo ne delitsya na 7 i na 23 bez ostatka!");
