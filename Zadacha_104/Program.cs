70d6412fa222b439aa3980d37c7bc2845cf74847
SCXBelka <scvifixbelka@gmail.com>
2022-12-12 22:24:58 +0900

    zadacha pro bolshyuy cifry v chisle

---
+++ Zadacha_104/Program.cs
@@ -0,0 +1,7 @@
+﻿int chislo = new Random().Next(10,100);
+Console.WriteLine(chislo);
+int numa = chislo%10;
+int numb = chislo/10;
+double max = numa;
+if (numa<numb) max =numb;
+Console.WriteLine(max);
