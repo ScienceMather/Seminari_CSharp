c992d95fe7ac63992e20ebea1c256719496143c1
SCXBelka <scvifixbelka@gmail.com>
2022-12-13 13:05:42 +0900

    naychilsya primenyat metodi

---
+++ Zadacha_209/Program.cs
@@ -0,0 +1,23 @@
+﻿
+int EnterData(string text_cord)
+{
+    Console.WriteLine(text_cord);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+int FindQuad(int x,int y)
+{
+    int result =0;
+    if (x>0 && y>0) result = 1;
+    if (x<0 && y>0) result = 2;
+    if (x<0 && y<0) result = 3;
+    if (x>0 && y<0) result = 4;
+    return result;
+}
+
+int x = EnterData ("Vvedite koordinaty X: ");
+int y = EnterData ("Vvedite koordinaty Y: ");
+
+int FindNum = FindQuad(x,y);
+Console.WriteLine($"Vashi Coordinati nahodyatsya v {FindNum} kvadrate");
