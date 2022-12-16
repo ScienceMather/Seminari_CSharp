6a07f9eb6629c6251fd7d43e99d24724e941cf49
SCXBelka <scvifixbelka@gmail.com>
2022-12-16 23:12:39 +0900

    programa dlya vichisleniya summi ot 1 do N

---
+++ Zadacha_313/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int SummNumb(int a)
+{
+    int sumka =0;
+    for (int i = 0; i <=a; i++)
+    {
+        sumka= sumka+i;
+    }
+    return sumka;
+
+}
+
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+
+int chislo = GetNumber("Vvedite chislo: ");
+int summ = SummNumb(chislo);
+Console.WriteLine($"Summa:{summ}");
