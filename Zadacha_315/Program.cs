e0ee84696f6be7e4589e843ca2e6a5f1869578b1
SCXBelka <scvifixbelka@gmail.com>
2022-12-16 23:17:59 +0900

    programa dlya vichisleniya proiz-niya ot 1 do N

---
+++ Zadacha_315/Program.cs
@@ -0,0 +1,21 @@
+﻿
+int SummNumb(int a)
+{
+    int pro =1;
+    for (int i = 1; i <=a; i++) pro= pro*i;
+    return pro;
+}
+
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+
+int chislo = GetNumber("Vvedite chislo: ");
+int prov = SummNumb(chislo);
+Console.WriteLine($"Proizvedenie: {prov}");
