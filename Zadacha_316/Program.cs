98404eacc4aa6ea7662aa390fdd4acd308243a3b
SCXBelka <scvifixbelka@gmail.com>
2022-12-16 23:20:32 +0900

    naychilsya sozdavat massivi

---
+++ Zadacha_316/Program.cs
@@ -0,0 +1,8 @@
+﻿int [] mass = new int [8];
+for (int i = 0; i < 8; i++)
+{
+    mass[i] = new Random().Next(0,2);
+    // Console.Write($"{mass[i]} ");
+}
+Console.WriteLine($"[{string.Join(",",mass)}]");
+
