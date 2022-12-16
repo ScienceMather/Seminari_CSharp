edc290a255a5812f851aaede3e6384cf9abc9a0d
SCXBelka <scvifixbelka@gmail.com>
2022-12-16 23:15:13 +0900

    zadacha dlya vichisleniya dlini chisla

---
+++ Zadacha_314/Program.cs
@@ -0,0 +1,29 @@
+﻿
+int SummNumb(int a)
+{
+    int count =0;
+    while (true)
+    {
+        if (a!=0)
+        {
+            a= a/10;
+            count++;
+        }
+        else break;
+    }
+    return count;
+
+}
+
+
+int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int chislo = GetNumber("Vvedite chislo: ");
+int schetchik = SummNumb(chislo);
+Console.WriteLine($"dlina chisla = {schetchik}");
