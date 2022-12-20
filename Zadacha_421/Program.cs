178ffc7b560ce816591552b136b3fa204883a38e
SCXBelka <scvifixbelka@gmail.com>
2022-12-20 22:42:13 +0900

    proizvedenie 1n,2n-1,3n-2...

---
+++ Zadacha_421/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int[] CreateMass(int len, int start,int end)
+{
+    int [] sumass= new int [len];
+    for (int i = 0; i < len; i++)
+    {
+        sumass[i] = new Random().Next(start,end+1);
+    }
+    return sumass;
+}
+
+int [] Pr_ind(int [] mass)
+{
+    int srez = mass.Length/2+mass.Length%2;
+    int [] mass_Pr =new int[srez];
+    for (int i =0;i<srez;i++)
+    {
+        int ind =mass[i]*mass[mass.Length-i-1];
+        mass_Pr[i]=ind;
+    }
+    return mass_Pr;
+}
+
+int size_mass = new Random().Next(5,11);
+int [] massiv = CreateMass(size_mass,1,5);
+Console.WriteLine($"[{string.Join(",",massiv)}]");
+int [] PR = Pr_ind(massiv);
+Console.WriteLine($"[{string.Join(",",PR)}]");
+
