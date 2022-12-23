b9d6914fe55d70b3707acbbcd7bf2faf016a6f09
SCXBelka <scvifixbelka@gmail.com>
2022-12-24 01:01:56 +0900

    zadacha pro sushestvovanie treygolnica

---
+++ Zadacha_523/Program.cs
@@ -0,0 +1,20 @@
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+bool Len_Triangle(int aa,int bb,int cc)
+{
+    bool result=false;
+    if (aa<bb+cc && bb<aa+cc && cc<bb+aa) result=true;
+    return result;
+}
+
+int a = GetNumber("Vvedite chislo a: ");
+int b = GetNumber("Vvedite chislo b: ");
+int c = GetNumber("Vvedite chislo c: ");
+if (Len_Triangle(a,b,c)) Console.Write($"Treugolnic so storonami {a} {b} {c} sushestvuet");
+else Console.Write($"Treugolnic so storonami {a} {b} {c} ne sushestvuet");
+
