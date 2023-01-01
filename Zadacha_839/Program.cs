5cc35d82d2922545b017b723055e00084be8ebc5
SCXBelka <scvifixbelka@gmail.com>
2023-01-01 23:23:10 +0900

    zadacha pro vozvedenie v stepen rekyrsiya

---
+++ Zadacha_839/Program.cs
@@ -0,0 +1,24 @@
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int Rekyrsion(int ch,int st)
+{
+    if (st==1) return ch;
+    else
+    {
+        ch=ch*Rekyrsion(ch,st-1);
+        Console.Write($"{ch} ");
+        return ch;
+    }
+    
+}
+
+int chislo =GetNumber("VVedite chislo: ");
+int stepen =GetNumber("VVedite stepen: ");
+int result =Rekyrsion(chislo,stepen);
+// Console.WriteLine($"{result}");
