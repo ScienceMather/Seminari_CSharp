e5931ffa7d62182c8ae905176bb00549b4741809
SCXBelka <scvifixbelka@gmail.com>
2023-01-01 23:20:22 +0900

    zadacha pro interval rekursii

---
+++ Zadacha_837/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int Rekyrsion(int start,int end)
+{
+    if (start==end) return start;
+    else
+    {
+        Console.Write($"{Rekyrsion(start,end-1)},");
+        return end;
+    }
+    
+}
+
+int start =GetNumber("VVedite SN: ");
+int end =GetNumber("VVedite EN: ");
+Rekyrsion(start,end);
+Console.Write(end);
