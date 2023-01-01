4665b8acc6e9591c993abae7d0dd16aa3a9be0ff
SCXBelka <scvifixbelka@gmail.com>
2023-01-01 23:21:42 +0900

    zadacha pro symmy cifr chisla rekyrsiya

---
+++ Zadacha_838/Program.cs
@@ -0,0 +1,23 @@
+﻿int GetNumber(string text)
+{
+    Console.WriteLine(text);
+    int numb =int.Parse(Console.ReadLine());
+    return numb;
+}
+
+
+int Rekyrsion(int rek)
+{
+    if (rek==0) return rek;
+    else
+    {
+        rek=rek%10+Rekyrsion(rek/10);
+        Console.Write($"{rek} ");
+        return rek;
+    }
+    
+}
+
+int rekyrs =GetNumber("VVedite N: ");
+int result =Rekyrsion(rekyrs);
+// Console.WriteLine($"{result}");
